50b685c194bda85a120eae583663615bbde591ee
Yiorgos <yiorgos.bagakis@protonmail,com>
2022-12-14 00:18:04 +0100

    3-commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-random-words/",
+  base: "https://yiorgosbagakis.github.io/react-random-words/",
   plugins: [react()],
 })
